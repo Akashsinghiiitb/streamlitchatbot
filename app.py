408d4acccfbe0e093b05ca5c9e25d157683c7fbc
Akashsinghiiitb <akashsinghlalla4@gmail.com>
2026-06-02 01:27:16 +0530

    last commit

--- app.py
+++ app.py
@@ -11,8 +11,14 @@ llm = HuggingFaceEndpoint(
 model = ChatHuggingFace(llm=llm) 
 
 st.header("this chatbot is developed by Akash Singh")
-user_input=st.text_input("Enter your prompt")
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+#user_input=st.text_input("Enter your prompt")
 
 if st.button("click here"):
-    result=model.invoke(user_input)
-    st.write(result.content)
+    #result=model.invoke(user_input)
+    st.write("hello")
